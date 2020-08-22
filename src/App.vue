ce304e311da68b001962d44e4216e9cdc8a1cf61
James Summers <227wLmrlcY0a>
2020-08-22 16:38:08 -0500

    update styles to scss

--- src/App.vue
+++ src/App.vue
@@ -18,42 +18,33 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
   html {
-    /* background-color: black; */
     height: 100vh;
     overflow: hidden;
     width: 100vw;
-  }
-  body {
-    background-color: gray;
-    display: flex;
-    margin: 0;
-  }
-  svg {
-    height: 100%;
-    width: 100%;
-  }
-  ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-  }
-  li {
-    list-style-type: none;
-  }
-  a {
-    text-decoration: none;
-  }
-  #app {
-    background-color: #f6f5f3;
-    color: #000000;
-    display: block;
-    font-family: 'NHaasGroteskTXStd-55Rg';
-    height: 100vh;
-    margin: 0 auto;
-    max-width: 1400px;
-    text-align: center;
-    width: 100vw;
+    body {
+      background-color: #a6a19a;
+      display: flex;
+      margin: 0;
+      svg {
+        height: 100%;
+        width: 100%;
+      }
+      a {
+        text-decoration: none;
+      }
+      #app {
+        background-color: #f6f5f3;
+        color: #000000;
+        display: block;
+        font-family: 'NHaasGroteskTXStd-55Rg';
+        height: 100vh;
+        margin: 0 auto;
+        max-width: 1920px;
+        text-align: center;
+        width: 100vw;
+      }
+    }
   }
 </style>
