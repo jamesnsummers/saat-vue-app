0c194d323b87b6f6818120350464bbcd78713171
James Summers <227wLmrlcY0a>
2020-08-22 15:19:57 -0500

    clean up code

--- src/main.js
+++ src/main.js
@@ -4,19 +4,5 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
-  methods: {
-    // checkUrlAndRoute: function() {
-    //   if (window.location.pathname === '/livestream' || window.location.pathname === '/livestream/') {
-    //     this.$el.querySelector('#livestream').click()
-      // } else if (window.location.pathname === '/art-direction' || window.location.pathname === '/art-direction/') {
-      //   this.$el.querySelector('#art_direction').click()
-    //   } else if (window.location.pathname === '/results' || window.location.pathname === '/results/') {
-    //     this.$el.querySelector('#stats').click()
-    //   }
-    // },
-  },
-  mounted() {
-    // this.checkUrlAndRoute()
-  },
   render: h => h(App),
 }).$mount('#app')
